858655af03fbc2460d61920e4ad4517b7f7e17ad
yadavashishdhirendra <85459137+DiTechCDM@users.noreply.github.com>
2022-09-19 09:55:04 +0530

    Finally Done

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Home.css";
+import MetaData from "../Helmet/MetaData";
+import CreateClient from "../CreateClient/CreateClient";
+import CreateList from "../DataCollections/ClientList";
+import Header from "../Header/Header";
+import TaskFromTeam from "../CreateTask/TaskFromTeam";
+
+const Home = () => {
+  return (
+    <>
+      <MetaData title={`Home - Create Client`} />
+      <Header />
+      {/* CREATE CLIENT */}
+      <CreateClient />
+      {/* CREATE CLIENT */}
+
+      {/* DATACOLLECTIONS */}
+      <h3 className="task-team">Client List</h3>
+      <CreateList />
+      <h3 className="task-team">Task Assigned From Team</h3>
+      <TaskFromTeam />
+      {/* DATACOLLECTIONS */}
+    </>
+  );
+};
+
+export default Home;
